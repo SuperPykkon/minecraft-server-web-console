0e2c83743e7d5091640f2098a7bd2752309584b6
SuperPykkon <SuperPykkon@users.noreply.github.com>
2016-07-30 17:09:46 +0530

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define("SERVER_NAME", "LegacyLands");
+define("SERVER_IP", "127.0.0.1");
+define("SERVER_PORT", "25565");
+
+define("SERVER_ROOT_DIR", "server/");
+define("SERVER_LOG_DIR", SERVER_ROOT_DIR . "logs/latest.log");
+
+?>
